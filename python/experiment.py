2e5e41a46c5cad2275b1c4fa48627adc7ff4d39e
Sara04 <sara.sedlar@gmail.com>
2017-01-29 12:20:27 +0100

    Experiment script cleaning

--- python/experiment.py
+++ python/experiment.py
@@ -1,89 +1,73 @@
+"""Experiment LiTS."""
 import os
-import numpy as np
-import nibabel as nib
+import sys
+import argparse
 from database import LiTSdb
-from preprocessor import Preprocessor
-from classifier import CNNClassifier
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from region_detectors import *
-from organ_detectors import *
+from scan import LiTSscan
+from preprocessor import LiTSpreprocessor
+from rough_detector import LiTSroughDetector
+from tumor_detector import LiTStumorDetector
 import time
 
-MIN_BODY_SLICE_RATIO = 0.2
 
-def main():	
+def main():
+    """Main function for the liver tumor detection training and testing."""
+    # Input arguments parsing
+    # _______________________________________________________________________ #
+    parser = argparse.ArgumentParser(description=''
+                                     'Liver tumor segmentation experiment')
+    parser.add_argument('-i', dest='db_path', required=True,
+                        help='Path to the directory where folders '
+                             '"Training Batch 1" and '
+                             '"Training Batch 2" are placed')
+    args = parser.parse_args()
+    if not os.path.exists(args.db_path):
+        print("\nInput database path does not exist!\n")
+        sys.exit(1)
+    if not os.path.exists(os.path.join(args.db_path, 'Training Batch 1')):
+        print("\nTraining Batch 1 is not in the input database directory!\n")
+        sys.exit(1)
+    if not os.path.exists(os.path.join(args.db_path, 'Training Batch 2')):
+        print("\nTraining Batch 2 is not in the input database direcory!\n")
+    # _______________________________________________________________________ #
 
-	path_to_database = '/home/sara/science/databases/LiTS/Training Batch 1/Training Batch 1'
+    # Database object creation and data split creation.
+    # _______________________________________________________________________ #
+    db = LiTSdb(args.db_path)
+    subjects = db.get_subjects_names()
+    training, validation, testing = db.get_data_split(subjects, 1)
+    # _______________________________________________________________________ #
 
-	db = LiTSdb(path_to_database)
-	preprocess = Preprocessor()
+    # Data loading, preprocessing and liver bounding box detection
+    # _______________________________________________________________________ #
+    preprocess = LiTSpreprocessor()
+    rough_detector = LiTSroughDetector()
+    scans = {}
+    time_start = time.time()
+    for s in subjects[0:]:
+        print("s:", s)
+        if s not in scans:
+            scans[s] = LiTSscan(s)
 
-	subjects = db.get_subjects_name()
-	data = {}
+        # Loading CT volume, segmentation labels and voxel size,
+        # volume intensity preprocessing and patient orientation
+        # ___________________________________________________________________ #
+        scans[s].load_volume(db.get_volume_path(s))
+        scans[s].load_segmentation(db.get_segmentation_path(s))
+        scans[s].load_info(db.get_volume_path(s))
+        preprocess.preprocess(scans[s], s)
+        rough_detector.detect_liver_bbox(scans[s])
 
-	for s in subjects[0:]:
+    time_end = time.time()
+    print("time elapsed:", time_end - time_start)
+    # _______________________________________________________________________ #
 
-		print("s:", s)
+    # Segmentation training and testing
+    # _______________________________________________________________________ #
+    tumor_detector = LiTStumorDetector()
 
-		if s not in data:
-			data[s] = {}
-
-		# 1. Loading CT volume, segmentation and voxel size
-		#________________________________________________________________________________________________________________________#
-		volume = db.load_volume(s)
-		segmentation = db.load_segmentation(s)
-		volume = preprocess.preprocess(volume)
-		voxel_size = db.load_voxel_size(s)
-		h, w, n = volume.shape
-		#________________________________________________________________________________________________________________________#
-		# 2. Detecting liver tissue
-		#________________________________________________________________________________________________________________________#
-		lung_detected_sa = False
-		for i in range(1, n):
-			# 2.1. Find body bbox in short axis view
-			#____________________________________________________________________________________________________________________#
-			x_start, x_end, y_start, y_end = body_bbox_sa(volume[:, :, n - i])
-			ratio = float(y_end - y_start) * float(x_end - x_start) / (h * w)
-			if ratio < MIN_BODY_SLICE_RATIO:
-				continue
-			#____________________________________________________________________________________________________________________#
-			# 2.2 Detect liver (liver initialization)
-			#____________________________________________________________________________________________________________________#
-			if not lung_detected_sa:
-				# 2.2.1 Set right lung approximate bbox and try to detect large lung region
-				#________________________________________________________________________________________________________________#
-				x_start_rl_sa, x_end_rl_sa, y_start_rl_sa, y_end_rl_sa = [x_start, x_end, y_start, (y_start + y_end) / 2]
-				right_lung_bbox_sa = extract_bbox(volume[:, :, n - i], x_start_rl_sa, x_end_rl_sa, y_start_rl_sa, y_end_rl_sa)
-				lung_detected_sa, lung_region_sa = lung_detection_sa(right_lung_bbox_sa, voxel_size)
-				#________________________________________________________________________________________________________________#
-			else:
-				# 2.2.2 Set right lung approximate bbox and try to detect large lung region
-				#________________________________________________________________________________________________________________#
-				h_rl_sa, w_rl_sa = lung_region_sa.shape
-				xc_rl_sa, yc_rl_sa = region_center(lung_region_sa)
-				right_lung_bbox_lap = extract_bbox(volume[y_start_rl_sa + yc_rl_sa * h_rl_sa, :, :], 0, -1, x_start_rl_sa, x_end_rl_sa)
-				lung_region_lap = lung_detection_lap(right_lung_bbox_lap, n - i, yc_rl_sa * w_rl_sa, voxel_size)
-				plt.figure(1)
-				plt.imshow(lung_region_lap)
-				plt.show()
-				break
-				#________________________________________________________________________________________________________________#
-		#________________________________________________________________________________________________________________________#
-
-		data[s]['volume'] = volume
-		data[s]['segmentation'] = segmentation
-		data[s]['voxel_size'] = voxel_size
-
-
-	s_train = [subjects[i] for i in range(0, 20)]
-	s_valid = [subjects[i] for i in range(20, 26)]
-	s_test = [subjects[i] for i in range(26, 27)]
-
-	cnn = CNNClassifier()
-
-	cnn.train(data, s_train, s_valid)
-	cnn.test(data, s_test)
+    tumor_detector.train(scans, training, validation)
+    tumor_detector.test(scans, testing)
 
 if __name__ == '__main__':
-	main()
+    main()
