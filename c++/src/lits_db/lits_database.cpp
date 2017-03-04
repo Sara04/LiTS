906639f8050ecc327226eaa7de2a39d59f8a2365
Sara04 <sara.sedlar@gmail.com>
2017-03-04 17:42:54 +0100

    [database]:database update, adding functions for testing database

--- c++/src/lits_db/lits_database.cpp
+++ c++/src/lits_db/lits_database.cpp
@@ -2,25 +2,28 @@
 
 // Training batches of the database
 // First contains 28 subjects and second 103
-std::string db_batches[2] = {"Training Batch 1",
-		                     "Training Batch 2"};
+std::string train_db_batches[2] = {"Training Batch 1",
+		                           "Training Batch 2"};
 
-// Possible database split ratios in training
-// validation and test sub-databases in percents
+// Testing batch of the database
+std::string test_batch = "Testing Batch";
+
+// Possible database split ratios into development
+// validation and evaluation subsets in percents
 unsigned short split_ratios[3][3] = {{60, 20, 20},
 		                             {50, 25, 25},
 		                             {80, 10, 10}};
 
 // Number of necessary database splits in order to
-// test algorithm on all data in such a way that
-// there is no overlap between training, validation
-// and testing sub-databases. (e.g. for the first
+// evaluate algorithm on all data in such a way that
+// there is no overlap between development, validation
+// and evaluation subsets. (e.g. for the first
 // split ratio and for the first data splitting
-// first 60% of the data is used for training,
-// following 20% for validation and 20% for test,
+// first 60% of the data is used for development,
+// following 20% for validation and 20% for evaluation,
 // then we shift data selection for 20%, and repeat
-// the same until the algorithm is tested on all data)
-unsigned short n_train_valid_test[3] = {5, 4, 10};
+// the same until the algorithm is evaluated on all data)
+unsigned short n_develop_valid_eval[3] = {5, 4, 10};
 
 /*
  * LiTS_db constructor: assigning database path
@@ -33,30 +36,20 @@ unsigned short n_train_valid_test[3] = {5, 4, 10};
 LiTS_db::LiTS_db(std::string db_path_)
 {
 	db_path = db_path_;
-	n_subjects = 0;
 	n_train = 0;
+	n_develop = 0;
 	n_valid = 0;
+	n_eval = 0;
 	n_test = 0;
 }
 
 /*
- * ~LiTS_db destructor - deleting/reseting all class attributes
- * and emptying all variables used for subject names storing
+ * load_train_subject_names - loading all training subject names into
+ * the private member training_names (vector of strings) and shuffling
+ * their order in order to avoid having data ordering by the institutions
+ * they were acquired in
  */
-LiTS_db::~LiTS_db()
-{
-	db_path = "";
-	n_subjects = 0;
-	subject_names.clear();
-	empty_split();
-}
-
-/*
- * load_subject_names - loading all subject names into the private member
- * subject_names (vector of strings) and shuffling their order in order to
- * avoid having data ordered by the institutions they were acquired in
- */
-void LiTS_db::load_subject_names()
+void LiTS_db::load_train_subject_names()
 {
 	fs::directory_iterator end_iter;
 	std::string subject_name;
@@ -64,94 +57,124 @@ void LiTS_db::load_subject_names()
 
 	for(unsigned int i = 0; i < 2; i++)
 	{
-		for(fs::directory_iterator dir_iter(db_path + "/" + db_batches[i]);
+		for(fs::directory_iterator dir_iter(db_path + "/" +
+				                            train_db_batches[i]);
 			dir_iter != end_iter;
 			++dir_iter)
 		{
 			if (!strncmp(basename(dir_iter->path()).c_str(), "volume", 6))
 			{
-				subject_name = basename(dir_iter->path());
-				subject_name = subject_name.substr(subject_name.find("-") + 1);
-				if(!subject_names.size())
-					subject_names.push_back(subject_name);
+				subject_name = basename(dir_iter->path()).substr(7);
+				if(!training_subjects.size())
+					training_subjects.push_back(subject_name);
 				else
 				{
-					for(it=subject_names.begin();
-						it < subject_names.end();
+					for(it=training_subjects.begin();
+						it < training_subjects.end();
 						it++)
 					{
 						if(std::atoi((*it).c_str()) >
 						   std::atoi(subject_name.c_str()))
 						{
-							subject_names.insert(it, subject_name);
+							training_subjects.insert(it, subject_name);
 							break;
 						}
 					}
-					if(it == subject_names.end())
-						subject_names.push_back(subject_name);
+					if(it == training_subjects.end())
+						training_subjects.push_back(subject_name);
 				}
 			}
 		}
 	}
 	std::srand(0);
-	std::random_shuffle(subject_names.begin(), subject_names.end());
-	n_subjects =  subject_names.size();
+	std::random_shuffle(training_subjects.begin(), training_subjects.end());
+	n_train =  training_subjects.size();
 }
 
+
 /*
- * data_split: splitting data into training, validation and testing parts
+ * load_test_subject_names - loading all testing subject names into
+ * the private member testing_names (vector of strings)
+ */
+void LiTS_db::load_test_subject_names()
+{
+
+	fs::directory_iterator end_iter;
+	std::string subject_name;
+	std::vector<std::string>::iterator it;
+
+	for(fs::directory_iterator dir_iter(db_path + "/" + test_batch);
+		dir_iter != end_iter;
+		++dir_iter)
+	{
+		subject_name = basename(dir_iter->path()).substr(12);
+		if(!testing_subjects.size())
+			testing_subjects.push_back(subject_name);
+		else
+		{
+			for(it=testing_subjects.begin();
+				it < testing_subjects.end();
+				it++)
+			{
+				if(std::atoi((*it).c_str()) >
+				   std::atoi(subject_name.c_str()))
+				{
+					testing_subjects.insert(it, subject_name);
+					break;
+				}
+			}
+			if(it == testing_subjects.end())
+				testing_subjects.push_back(subject_name);
+		}
+	}
+}
+/*
+ * train_data_split: splitting data into development, validation and
+ * evaluation parts
  *
  * Arguments:
  * 		split_ratio: selection of one of the the three offered split ratios
  * 		selection: selection of the training split ordinal number for the
  * 		given split ratio
  */
-void LiTS_db::data_split(int split_ratio, int selection)
+void LiTS_db::train_data_split(int split_ratio, int selection)
 {
 	empty_split();
-	int n = float(subject_names.size()) / n_train_valid_test[split_ratio];
-	n_train = split_ratios[split_ratio][0] /
-			  (100 / n_train_valid_test[split_ratio]) * n;
+	int n = float(n_train) / n_develop_valid_eval[split_ratio];
+	n_develop = split_ratios[split_ratio][0] /
+			  (100 / n_develop_valid_eval[split_ratio]) * n;
 	n_valid = split_ratios[split_ratio][1] /
-			  (100 / n_train_valid_test[split_ratio]) * n;
-	n_test = subject_names.size() - n_train - n_valid;
-	for(unsigned int i = 0; i < n_train; i++)
-		training_subjects.push_back(subject_names.
-				at((selection * n_test + i) % n_subjects));
+			  (100 / n_develop_valid_eval[split_ratio]) * n;
+	n_eval = n_train - n_develop - n_valid;
+	for(unsigned int i = 0; i < n_develop; i++)
+		development_subjects.push_back(training_subjects.
+				at((selection * n_eval + i) % n_train));
 	for(unsigned int i = 0; i < n_valid; i++)
-		validation_subjects.push_back(subject_names.
-				at((selection * n_test + n_train + i) % n_subjects));
-	for(unsigned int i = 0; i < n_test; i++)
-		testing_subjects.push_back(subject_names.
-				at((selection * n_test + n_train + n_valid + i) % n_subjects));
+		validation_subjects.push_back(training_subjects.
+				at((selection * n_eval + n_develop + i) % n_train));
+	for(unsigned int i = 0; i < n_eval; i++)
+		evaluation_subjects.push_back(training_subjects.
+				at((selection * n_eval + n_develop + n_valid + i) % n_train));
 }
 
 /*
- * empty_split: reseting the number of training, validation and testing scans
- * and emptying vectors of training, validation and testing scans
+ * empty_split: reseting the number and emptying lists of
+ * development, validation and evaluation subjects
  */
 void LiTS_db::empty_split()
 {
 	n_train = 0;
 	n_valid = 0;
 	n_test = 0;
-	training_subjects.clear();
+	development_subjects.clear();
 	validation_subjects.clear();
-	testing_subjects.clear();
+	evaluation_subjects.clear();
 }
 
-/*
- * get_number_of_subjects: returning the total number of subjects/scans in the
- * database
- */
-int LiTS_db::get_number_of_subjects()
-{
-	return n_subjects;
-}
 
 /*
- * get_number_of_training: returning the total number of subjects/scans in the
- * training sub-database
+ * get_number_of_training: returning the total number of subjects in
+ * the training database
  */
 int LiTS_db::get_number_of_training()
 {
@@ -159,8 +182,17 @@ int LiTS_db::get_number_of_training()
 }
 
 /*
- * get_number_of_validation: returning the total number of subjects/scans in
- * the validation sub-database
+ * get_number_of_development: returning the total number of subjects in
+ * the development subset
+ */
+int LiTS_db::get_number_of_development()
+{
+	return n_develop;
+}
+
+/*
+ * get_number_of_validation: returning the total number of subjects in
+ * the validation subset
  */
 int LiTS_db::get_number_of_validation()
 {
@@ -168,8 +200,17 @@ int LiTS_db::get_number_of_validation()
 }
 
 /*
- * get_number_of_testing: returning the total number of subjects/scans in the
- * testing sub-database
+ * get_number_of_evaluation: returning the total number of subjects in
+ * the evaluation subset
+ */
+int LiTS_db::get_number_of_evaluation()
+{
+	return n_eval;
+}
+
+/*
+ * get_number_of_testing: returning the total number of subjects in
+ * the testing database
  */
 int LiTS_db::get_number_of_testing()
 {
@@ -177,14 +218,13 @@ int LiTS_db::get_number_of_testing()
 }
 
 /*
- * get_train_scan_name: returning the subject's/scan's name at the required
- * position in the training sub-database
+ * get_train_subject_name: returning the subject's name at the required
+ * position in the training database
  *
  * Arguments:
- * 		position: required subject's/scan's position in the training
- * 		sub-database
+ * 		position: required subject's position in the training database
  */
-std::string LiTS_db::get_train_scan_name(int position)
+std::string LiTS_db::get_train_subject_name(int position)
 {
 	if (position < n_train and position >= 0)
 		return training_subjects.at(position);
@@ -197,14 +237,32 @@ std::string LiTS_db::get_train_scan_name(int position)
 }
 
 /*
- * get_valid_scan_name: returning the subject's/scan's name at the required
- * position in the validation sub-database
+ * get_develop_subject_name: returning the subject's name at the required
+ * position in the development subset
  *
  * Arguments:
- * 		position: required subject's/scan's position in the validation
- * 		sub-database
+ * 		position: required subject's position in the development subset
  */
-std::string LiTS_db::get_valid_scan_name(int position)
+std::string LiTS_db::get_develop_subject_name(int position)
+{
+	if (position < n_develop and position >= 0)
+		return development_subjects.at(position);
+	else
+	{
+		std::cout<<"Position:"<<position<<std::endl;
+		std::cout<<"warning: Invalid element selection"<<std::endl;
+		return NULL;
+	}
+}
+
+/*
+ * get_valid_subject_name: returning the subject's name at the required
+ * position in the validation subset
+ *
+ * Arguments:
+ * 		position: required subject's position in the validation subset
+ */
+std::string LiTS_db::get_valid_subject_name(int position)
 {
 	if (position < n_valid and position >= 0)
 		return validation_subjects.at(position);
@@ -217,14 +275,32 @@ std::string LiTS_db::get_valid_scan_name(int position)
 }
 
 /*
- * get_test_scan_name: returning the subject's/scan's name at the required
- * position in the testing sub-database
+ * get_eval_subject_name: returning the subject's name at the required
+ * position in the evaluation subset
  *
  * Arguments:
- * 		position: required subject's/scan's position in the testing
- * 		sub-database
+ * 		position: required subject's position in the evaluation subset
  */
-std::string LiTS_db::get_test_scan_name(int position)
+std::string LiTS_db::get_eval_subject_name(int position)
+{
+	if (position < n_eval and position >= 0)
+		return evaluation_subjects.at(position);
+	else
+	{
+		std::cout<<"Position:"<<position<<std::endl;
+		std::cout<<"warning: Invalid element selection"<<std::endl;
+		return NULL;
+	}
+}
+
+/*
+ * get_test_subject_name: returning the subject's name at the required
+ * position in the testing database
+ *
+ * Arguments:
+ * 		position: required subject's position in the testing database
+ */
+std::string LiTS_db::get_test_subject_name(int position)
 {
 	if (position < n_test and position >= 0)
 		return testing_subjects.at(position);
@@ -236,29 +312,45 @@ std::string LiTS_db::get_test_scan_name(int position)
 	}
 }
 
+
 /*
- * get_scan_paths: creating the volume and segmentation path of the
- * required subjec/scan
+ * get_train_paths: creating the volume and segmentation path for the
+ * required subject's name
  *
  * Arguments:
- * 		scan_name: string containing name of the subject/scan
+ * 		subject_name: string containing name of the training subject
  * 		volume_path: string where the volume path would be stored
- * 		segmentation_path: string where the segmentation path would be stored
+ * 		segmentation_path: string where the segmentation path would
+ * 			be stored
  */
-void LiTS_db::get_scan_paths(const std::string scan_name,
-                             std::string &volume_path,
-                             std::string &segmentation_path)
+void LiTS_db::get_train_paths(const std::string subject_name,
+                              std::string &volume_path,
+                              std::string &segmentation_path)
 {
 	std::string db_batch;
 
-	if (atoi(scan_name.c_str()) < 28)
+	if (atoi(subject_name.c_str()) < 28)
 		db_batch = "/Training Batch 1";
 	else
 		db_batch = "/Training Batch 2";
 
 	volume_path = db_path + db_batch +
-			      "/volume-" + scan_name + ".nii";
+			      "/volume-" + subject_name + ".nii";
 
 	segmentation_path = db_path + db_batch +
-			            "/segmentation-" + scan_name + ".nii";
+			            "/segmentation-" + subject_name + ".nii";
+}
+
+/*
+ * get_test_path: creating the volume path for the required subject's name
+ *
+ * Arguments:
+ * 		subject_name: string containing name of the testing subject
+ * 		volume_path: string where the volume path would be stored
+ */
+void LiTS_db::get_test_path(const std::string subject_name,
+                            std::string &volume_path)
+{
+	volume_path = db_path + "/Testing Batch" +
+			      "/volume-test-" + subject_name + ".nii";
 }
--- c++/src/lits_db/lits_database.h
+++ c++/src/lits_db/lits_database.h
@@ -18,81 +18,107 @@ namespace fs = boost::filesystem;
 
 /*
  * Class LiTS_db for database management.
- * It has means of creating list of all available subjects
- * in the database; splitting data into training, validation
- * and testing parts; getting the number of training, validation
- * and testing subjects; getting the subjects of the training,
- * validation and testing sub-databases; getting the volume and
- * segmentation paths of a scan.
+ * It has means of creating lists of all available subjects
+ * in the database from training and testing batches;
+ * splitting the training data into development, validation
+ * and evaluation parts; getting the number and subjects of the
+ * training, development, validation, evaluation and testing
+ * subsets;
+ * getting the volume and segmentation paths of a training scan;
+ * getting the volume path of a testing volume
  *
- * Arguments:
+ *
+ * Attributes:
  * 		db_path: path to the directory containing folders
- * 		         "Training Batch 1" and "Training Batch 2"
- * 		subject_names: vector for storing all subject/scan names
- * 		training_subjects: vector for storing all training subject/scan
- * 		                   names
- * 		validation_subjects: vector for storing all validation subject/scan
- * 		                     names
- * 		testing_subjects: vector for storing all testing subject/scan names
- * 		n_subjects: the total number of subjects/scans
- * 		n_train: the total number of training subjects/scans
- * 		n_valid: the total number of validation subjects/scans
- * 		n_test: the total number of testing subjects/scans
+ * 		         "Training Batch 1", "Training Batch 2" and
+ * 		         "Testing Batch"
+ * 		training_subjects: vector for storing subjects' names
+ * 			from the training batches
+ * 		development_subjects: vector for storing subjects' names
+ * 			for algorithm development/training
+ * 		validation_subjects: vector for storing subjects' names
+ * 			for algorithm validation
+ * 		evaluation_subjects: vector for storing subjects' names
+ * 			for algorithm evaluation
+ * 		testing_subjects: vector for storing subjects' names
+ * 			from the testing batch
+ *
+ * 		n_train: the total number of training subjects
+ * 		n_develop: the total number of development subjects
+ * 		n_valid: the total number of validation subjects
+ * 		n_eval: the total number of evaluation subjects
+ * 		n_test: the total number of testing subjects
  *
  * Methods:
  * 		LiTS_db: constructor
- * 		~LiTS_db: destructor
- * 		load_subject_names: loading all subjects'/scans' names
- * 		data_split: splitting data into training, validation and testing parts
+ * 		load_train_subject_names: loading train subjects' names
+ * 		load_test_subject_names: loading test subjects' names
+ * 		train_data_split: splitting data into development, validation
+ * 			and evaluation parts
  * 		empty_split: reseting data split
- * 		get_number_of_subjects: get the total number of loaded subjects/scans
- * 		get_number_of_training: get the total number of training subjects/
- * 		                        scans
- * 		get_number_of_validation: get the total number of validation subjects/
- * 		                          scans
- * 		get_number_of_testing: get the total number of testing subjects/scans
- * 		get_train_scan_name: get the subject/scan name from the training
- * 		                     subset
- * 		get_valid_scan_name: get the subject/scan name from the validation
- * 		                     subset
- * 		get_test_scan_name: get the subject/scan name from the testing subset
- * 		get_scan_paths: create volume and segmentation paths for a given scan
  *
+ * 		get_number_of_training: get the total number of training subjects
+ * 		get_number_of_development: get the total number of development subjects
+ * 		get_number_of_validation: get the total number of validation subjects
+ * 		get_number_of_evaluation: get the total number of evaluation subjects
+ * 		get_number_of_testing: get the total number of testing subjects
+ *
+ * 		get_train_subject_name: get the subject's name from the training set at
+ * 			required position
+ * 		get_develop_subject_name: get the subject's name from the development
+ * 			set at required position
+ * 		get_valid_subject_name: get the subject's name from the validation set
+ * 			at required position
+ * 		get_eval_subject_name: get the subject's name from the validation set
+ * 			at required position
+ * 		get_test_subject_name: get the subject's name from the testing set
+ * 			at required position
+ *
+ * 		get_train_paths: get subject's volume and segmentation paths
+ * 		get_test_path: get subject's volume path
  */
 class LiTS_db
 {
 private:
 	std::string db_path;
-	std::vector<std::string> subject_names;
 	std::vector<std::string> training_subjects;
+	std::vector<std::string> development_subjects;
 	std::vector<std::string> validation_subjects;
+	std::vector<std::string> evaluation_subjects;
 	std::vector<std::string> testing_subjects;
 
-	int n_subjects;
 	int n_train;
+	int n_develop;
 	int n_valid;
+	int n_eval;
 	int n_test;
 
 public:
 	LiTS_db(std::string db_path_);
-	~LiTS_db();
 
-	void load_subject_names();
-	void data_split(int split_ratio, int selection);
+	void load_train_subject_names();
+	void load_test_subject_names();
+	void train_data_split(int split_ratio, int selection);
 	void empty_split();
 
-	int get_number_of_subjects();
 	int get_number_of_training();
+	int get_number_of_development();
 	int get_number_of_validation();
+	int get_number_of_evaluation();
 	int get_number_of_testing();
 
-	std::string get_train_scan_name(int position);
-	std::string get_valid_scan_name(int position);
-	std::string get_test_scan_name(int position);
+	std::string get_train_subject_name(int position);
+	std::string get_develop_subject_name(int position);
+	std::string get_valid_subject_name(int position);
+	std::string get_eval_subject_name(int position);
+	std::string get_test_subject_name(int position);
 
-	void get_scan_paths(const std::string scan_name,
-			            std::string &volume_path,
-			            std::string &segmentation_path);
+	void get_train_paths(const std::string subject_name,
+			             std::string &volume_path,
+			             std::string &segmentation_path);
+
+	void get_test_path(const std::string subject_name,
+			           std::string &volume_path);
 };
 
 #endif /* LITS_DATABASE_H_ */
