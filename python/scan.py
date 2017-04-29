7a55e336fc50c7581dc8aa17be4cc14cd75d883a
Sara04 <sara.sedlar@gmail.com>
2017-04-29 15:15:27 +0200

    [scan]: update LiTSscan class, add new methods and attributes, add comments

--- python/scan.py
+++ python/scan.py
@@ -7,97 +7,175 @@ class LiTSscan(object):
     """LiTSscan class for scan loading and writing.
 
     Attributes:
-        name (str): scan name
-        volume (numpy array): 3d array where scan volume is stored
-        h (int): volume hight
-        w (int): volume width
-        d (int): volume depth
-        segmentation (numpy array): 3d array where ground truth labels
-                                    for liver and tumor are stored
-        voxel_size (list): list of floats that represent voxel hight,
-                           width and depth (slice distance)
-        orientation_info (tuple): tuple of three strings indicating
-                                  scaner's coordinate system
-        liver_bbox (list): 3d liver bounding box
+        volume_path: path to the volume data
+        segmentation_path: path to the ground truth segmentation data
+        meta_segmentation_path: path to the meta segmentation data
+
+        volume: volume data
+        segmentation: segmentation data
+        meta_segmentation: meta segmentation data
+
+        h: volume height (front-back body direction)
+        w: volume width (left-right body direction)
+        d: volume depth (bottom-top body direction)
+
+        h_voxel: voxel height (front-back body direction)
+        w_voxel: voxel width (left-right body direction)
+        d_voxel: voxel depth (bottom-top body direction)
+
+        axes_order: order of the volume's axes
+        axes_orientation: orientation of the volume's axes
+
     Methods:
         load_volume: loads and stores volume and its hight, width and depth
         load_segmentation: loads and stores ground truth segmentation
         load_info: loads and stores coordinate system orientation info and
                    voxel size
+
+        get_volume: get pointer to the volume data
+        get_segmentation: get pointer to the segmentation data
+        get_meta_segmentaion: get pointer to the meta segmentation data
+
+        get_height: get height of the volume/segmentation
+            (front-back body direction)
+        get_width: get width of the volume/segmentation
+            (left-right body direction)
+        get_depth: get depth of the volume/segmentation
+            (bottom-top body direction)
+
+        get_voxel_height: get height of the voxels
+        get_voxel_width: get width of the voxels
+        get_voxel_depth: get depth of the voxels (slice distance)
+
+        get_axes_order: get the order of the axes
+        get_axes_orientation: get the orientations of the axes
     """
 
-    def __init__(self, name):
+    def __init__(self, volume_path, segmentation_path=None,
+                 meta_segmentation_path=None):
         """Initialization method for LiTSscan object.
 
-        Args:
-            name (str): scan name
+        Arguments:
+            volume_path: path to the volume data
+            segmentation_path: path to the ground truth segmentation data
+            meta_segmentation_path: path to the meta segmentation data
         """
-        self.name = name
+        self.volume_path = volume_path
+        self.segmentation_path = segmentation_path
+        self.meta_segmentation_path = meta_segmentation_path
+
         self.volume = None
+        self.segmentation = None
+        self.meta_segmentation = None
+
         self.h = None
         self.w = None
         self.d = None
-        self.segmentation = None
-        self.voxel_size = None
-        self.orientation_info = None
-        self.liver_bbox = None
-        self.orientation = None
 
-    def load_volume(self, s_path):
-        """Load and store volume and its hight, width and depth.
+        self.voxel_h = None
+        self.voxel_w = None
+        self.voxel_d = None
 
-        Args:
-            s_path (str): path to the volume file
+        self.axes_order = None
+        self.axes_orientation = None
+
+    def load_volume(self):
+        """Load and store volume and its hight, width and depth."""
+        self.volume = np.array(nib.load(self.volume_path).get_data(),
+                               dtype='float32').transpose(1, 0, 2)
+
+    def load_segmentation(self):
+        """Load and store ground truth segmentation."""
+        self.segmentation =\
+            np.array(nib.load(self.segmentation_path).get_data(),
+                     dtype='uint8').transpose(1, 0, 2)
+
+    def load_info(self):
+        """Load scan's axes order and orientation, volume and voxel sizes."""
+        slice_data = nib.load(self.volume_path)
+        volume_d = slice_data.header.get_data_shape()
+        voxel_s = []
+        affine_m = slice_data.affine
+
+        self.axes_order = []
+        self.axes_orientation = []
+        for i in range(3):
+            for j in range(3):
+                if affine_m[i, j] != 0:
+                    self.axes_order.append(j)
+                    voxel_s.append((affine_m[i, j]))
+                    self.axes_orientation.append(2 * (affine_m[i, j] > 0) - 1)
+
+        self.w = volume_d[self.axes_order[0]]
+        self.h = volume_d[self.axes_order[1]]
+        self.d = volume_d[self.axes_order[2]]
+
+        self.voxel_w, self.voxel_h, self.voxel_d = voxel_s
+
+    def set_volume(self, volume):
+        """Set volume."""
         """
-        self.volume = (np.array(nib.load(s_path).get_data(),
-                                dtype='float32').
-                       transpose(1, 0, 2)[::-1, ::-1, :])
-
-        self.w, self.h, self.d = self.volume.shape
-
-    def load_segmentation(self, s_path):
-        """Load and store ground truth segmentation.
-
-        Args:
-            s_path (str): path to the segmentation file
+            Arguments:
+                volume: new volume data
         """
-        self.segmentation = (np.array(nib.load(s_path).get_data(),
-                                      dtype='uint8').
-                             transpose(1, 0, 2)[::-1, ::-1, :])
+        self.volume = volume
 
-    def load_info(self, s_path):
-        """Load and store scaner's coordinate system info and voxel size.
-
-        Args:
-            s_path (str): path to the volume file
+    def set_segmentation(self, segmentation):
+        """Set segmentation."""
         """
-        slice_data = nib.load(s_path)
-        self.orientation_info = nib.aff2axcodes(slice_data.affine)
-        self.voxel_size = [abs(slice_data.affine[i, i]) for i in range(3)]
-
-    def save_volume(self, s_path):
-        """Save volume - to be implemented.
-
-        Args:
-            s_path (str): output path to the volume
+            Arguments:
+                segmentation: new segmentation data
         """
-        raise NotImplementedError()
+        self.segmentation = segmentation
 
-    def save_segmentation(self, s_path):
-        """Save segmentation - to be implemented.
-
-        Args:
-            s_path (str): output path to the segmentation
+    def set_meta_segmentation(self, meta_segmentation):
+        """Set meta segmentation."""
         """
-        raise NotImplementedError()
+            Arguments:
+                meta_segmentation: new meta_segmentation data
+        """
+        self.meta_segmentation = meta_segmentation
 
-    def empty(self):
-        """Empty all scan variables except name."""
-        self.volume = None
-        self.h = None
-        self.w = None
-        self.d = None
-        self.segmentation = None
-        self.voxel_size = None
-        self.orientation_info = None
-        self.liver_bbox = None
+    def get_volume(self):
+        """Get volume data."""
+        return self.volume
+
+    def get_segmentation(self):
+        """Get segmentation data."""
+        return self.segmentation
+
+    def get_meta_segmentation(self):
+        """Get meta segmentation data."""
+        return self.meta_segmentation
+
+    def get_width(self):
+        """Get scan width."""
+        return self.w
+
+    def get_height(self):
+        """Get scan height."""
+        return self.h
+
+    def get_depth(self):
+        """Get scan depth."""
+        return self.d
+
+    def get_voxel_width(self):
+        """Get voxel width."""
+        return self.voxel_w
+
+    def get_voxel_height(self):
+        """Get voxel height."""
+        return self.voxel_h
+
+    def get_voxel_depth(self):
+        """Get voxel depth."""
+        return self.voxel_d
+
+    def get_axes_order(self):
+        """Get axes order."""
+        return self.axes_order
+
+    def get_axes_orientation(self):
+        """Get axes orientation."""
+        return self.axes_orientation
