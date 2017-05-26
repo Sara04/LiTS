f84060ac368a970a7eacb71b71214075952a9958
Sara04 <sara.sedlar@gmail.com>
2017-05-26 16:33:29 +0200

    [liver side estimation]: adding new methods, moving some parameters as input to the methods

--- c++/src/liver_side_estimation.cpp
+++ c++/src/liver_side_estimation.cpp
@@ -14,35 +14,61 @@
 
 #include "liver_side_estimation.h"
 
-void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db)
+void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
+                                          unsigned N_iters,
+                                          unsigned N_subj_t,
+                                          unsigned N_subj_v,
+                                          unsigned N_augment_t,
+                                          float lr)
 {
-    unsigned N_iters = 1000;
-    unsigned N_subj_batch = 5;
-    unsigned N_augment = 10;
     LiTS_liver_side_estimator liver_side_estimator(model_path);
     LiTS_processor p(-100, 400,-0.5, 0.5);
+
     float t_acc = 0;
     float v_acc = 0;
+
     struct timeval start_time, end_time;
     gettimeofday(&start_time, NULL);
     for(unsigned int it = 0; it < N_iters; it++)
     {
-        t_acc = liver_side_estimator.train_liver_side_estimator(db, p,
-                                                                N_iters,
-                                                                N_subj_batch,
-                                                                N_augment);
+        t_acc = liver_side_estimator.develop_liver_side_estimator(db, p,
+                                                                  N_iters,
+                                                                  N_subj_t,
+                                                                  N_augment_t,
+                                                                  lr,
+                                                                  true);
         if(it % 100 == 0)
-            v_acc = liver_side_estimator.valid_liver_side_estimator(db, p);
+            v_acc = liver_side_estimator.valid_liver_side_estimator(db, p,
+                                                                    N_subj_v,
+                                                                    true);
 
         std::cout<<"(Iteration: "<<(it + 1)<<"/"<<N_iters<<"), (valid/train):";
         std::cout<<" ("<<v_acc<<"/"<<t_acc<<")\r"<<std::flush;
     }
+    liver_side_estimator.save_model();
     gettimeofday(&end_time, NULL);
     float runtime = (end_time.tv_sec-start_time.tv_sec) +
                     (end_time.tv_usec-start_time.tv_usec) * 1e-6;
 
     std::cout<<std::endl;
-    std::cout<<"Time elapsed: "<<runtime<<std::endl;
-
+    std::cout<<"Time elapsed for liver side estimator model is: ";
+    std::cout<<runtime<<std::endl;
 }
 
+void liver_side_estimator_eval(std::string model_path, LiTS_db db,
+                               std::string scan_name)
+{
+    LiTS_liver_side_estimator liver_side_estimator(model_path);
+    LiTS_processor p(-100, 400,-0.5, 0.5);
+
+    liver_side_estimator.eval_liver_side_estimator(db, p, scan_name, true);
+}
+
+void estimate_liver_side(std::string model_path, LiTS_db db,
+                         std::string scan_name)
+{
+    LiTS_liver_side_estimator liver_side_estimator(model_path);
+    LiTS_processor p(-100, 400,-0.5, 0.5);
+
+    liver_side_estimator.estimate_liver_side(db, p, scan_name, true);
+}
--- c++/src/liver_side_estimation.h
+++ c++/src/liver_side_estimation.h
@@ -15,6 +15,18 @@
 #include "lits_processing/lits_processor.h"
 #include "liver_segmentation/liver_side_estimator.h"
 
-void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db);
+
+void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
+                                          unsigned N_iters=1000,
+                                          unsigned N_subj_t=5,
+                                          unsigned N_subj_v=5,
+                                          unsigned N_augment_t=10,
+                                          float lr=0.01);
+
+void liver_side_estimator_eval(std::string model_path, LiTS_db db,
+                               std::string scan_name);
+
+void estimate_liver_side(std::string model_path, LiTS_db db,
+                         std::string scan_name);
 
 #endif /* LIVER_SIDE_ESTIMATION_H_ */
