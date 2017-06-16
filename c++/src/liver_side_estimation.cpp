1f75acf88bf1a1b6cd9089a10ad83bd5ce61ad39
Sara04 <sara.sedlar@gmail.com>
2017-06-16 23:06:50 +0200

    [liver side estimation]:update functions that calls training and validation

--- c++/src/liver_side_estimation.cpp
+++ c++/src/liver_side_estimation.cpp
@@ -4,21 +4,23 @@
  *  Created on: May 21, 2017
  *      Author: sara
  */
-/******************************************************************************
- * This functions runs liver side estimation model training, validation and
- * saving. Since the information about orientation of the axis that is normal
- * to the sagittal plane is not reliable and it is considered to be important
- * for the further liver and tumor segmentation, a model is trained to estimate
- * its orientation (or liver's side).
- *****************************************************************************/
 
 #include "liver_side_estimation.h"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+/******************************************************************************
+ * save_liver_side_ground_truth: function of liver side ground truth
+ * calculation based on liver and lungs segmentation masks
+ *
+ * arguments:
+ * 		db: LiTS_db object
+ *****************************************************************************/
 void save_liver_side_ground_truth(LiTS_db db)
 {
-
     LiTS_liver_side_estimator liver_side_estimator;
-    LiTS_processor p(-100, 400,-0.5, 0.5);
+    LiTS_processor p(-200, 500,-0.5, 0.5);
     boost::progress_display show_progress( db.get_number_of_training() );
     for(unsigned i = 0; i < db.get_number_of_training() ; i++)
     {
@@ -29,7 +31,7 @@ void save_liver_side_ground_truth(LiTS_db db)
         db.get_train_meta_segment_path(scan_name, meta_segment_path);
         db.get_train_liver_side_gt_path(scan_name, liver_gt_path);
         LiTS_scan ls(volume_path, segment_path, meta_segment_path);
-        liver_side_estimator.load_and_preprocess_scan(p, ls);
+        liver_side_estimator.load_and_reorient_masks(p, ls);
 
         unsigned char ** mask_gt = new unsigned char*[1];
         unsigned char ** mask_m = new unsigned char*[1];
@@ -60,6 +62,23 @@ void save_liver_side_ground_truth(LiTS_db db)
     }
 }
 
+/******************************************************************************
+ * liver_side_estimator_train_and_valid: this function runs liver side
+ * estimation model training, validation and saving. Since the information
+ * about orientation of the axis that is normal to the sagittal plane is not
+ * reliable and it is considered to be important for the further liver and
+ * tumor segmentation, a model is trained to estimate its orientation
+ * (or liver's side)
+ *
+ * arguments:
+ * 		model_path: path where to store model
+ * 		db: LiTS_db object
+ * 		N_iters: number of iterations
+ * 		N_subj_t: number of subjects for training
+ * 		N_subj_v: number of subjects for validation
+ * 		N_augment_t: augmentation factor of training data
+ * 		lr: learning rate
+ *****************************************************************************/
 void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
                                           unsigned N_iters,
                                           unsigned N_subj_t,
@@ -81,8 +100,7 @@ void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
         t_acc = liver_side_estimator.develop_liver_side_estimator(db, p,
                                                                   N_subj_t,
                                                                   N_augment_t,
-                                                                  lr,
-                                                                  true);
+                                                                  lr, true);
         if(it % 20 == 0)
             v_acc = liver_side_estimator.valid_liver_side_estimator(db, p,
                                                                     N_subj_v,
@@ -90,8 +108,11 @@ void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
 
         std::cout<<"(Iteration: "<<(it + 1)<<"/"<<N_iters<<"), (valid/train):";
         std::cout<<" ("<<v_acc<<"/"<<t_acc<<")\r"<<std::flush;
+
+        if((it % 100) == 0)
+            liver_side_estimator.save_model(it);
     }
-    liver_side_estimator.save_model();
+
     gettimeofday(&end_time, NULL);
     float runtime = (end_time.tv_sec-start_time.tv_sec) +
                     (end_time.tv_usec-start_time.tv_usec) * 1e-6;
@@ -99,13 +120,14 @@ void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
     std::cout<<std::endl;
     std::cout<<"Time elapsed for liver side estimator model is: ";
     std::cout<<runtime<<std::endl;
+
 }
 
 void liver_side_estimator_eval(std::string model_path, LiTS_db db,
                                std::string scan_name)
 {
     LiTS_liver_side_estimator liver_side_estimator(model_path);
-    LiTS_processor p(-100, 400,-0.5, 0.5);
+    LiTS_processor p(-200, 500,-0.5, 0.5);
 
     liver_side_estimator.eval_liver_side_estimator(db, p, scan_name, true);
 }
@@ -114,7 +136,7 @@ void estimate_liver_side(std::string model_path, LiTS_db db,
                          std::string scan_name)
 {
     LiTS_liver_side_estimator liver_side_estimator(model_path);
-    LiTS_processor p(-100, 400,-0.5, 0.5);
+    LiTS_processor p(-200, 500,-0.5, 0.5);
 
     liver_side_estimator.estimate_liver_side(db, p, scan_name, true);
 }
--- c++/src/liver_side_estimation.h
+++ c++/src/liver_side_estimation.h
@@ -14,16 +14,16 @@
 #include "lits_scan/lits_scan.h"
 #include "lits_processing/lits_processor.h"
 #include "liver_segmentation/liver_side_estimator.h"
-#include "liver_segmentation/liver_side_estimator.cuh"
+#include "liver_segmentation/liver_common_methods.cuh"
 
 void save_liver_side_ground_truth(LiTS_db db);
 
 void liver_side_estimator_train_and_valid(std::string model_path, LiTS_db db,
-                                          unsigned N_iters=2000,
-                                          unsigned N_subj_t=4,
+                                          unsigned N_iters=10000,
+                                          unsigned N_subj_t=3,
                                           unsigned N_subj_v=10,
-                                          unsigned N_augment_t=10,
-                                          float lr=0.01);
+                                          unsigned N_augment_t=2,
+                                          float lr=0.001);
 
 void liver_side_estimator_eval(std::string model_path, LiTS_db db,
                                std::string scan_name);
